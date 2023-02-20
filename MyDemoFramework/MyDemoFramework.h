7cbf94b47d62872870b8fedd047e7df3ea393923
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-20 12:22:14 +0530

    Initial Commit

---
+++ MyDemoFramework/MyDemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyDemoFramework.h
+//  MyDemoFramework
+//
+//  Created by Sagar Kadam on 20/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDemoFramework.
+FOUNDATION_EXPORT double MyDemoFrameworkVersionNumber;
+
+//! Project version string for MyDemoFramework.
+FOUNDATION_EXPORT const unsigned char MyDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDemoFramework/PublicHeader.h>
+
+
